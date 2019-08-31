8a6b2d7705f6d62f960ab918490bac4dea14460b
Azamora16 <43578630+Azamora16@users.noreply.github.com>
2019-08-31 14:18:32 -0700

    Add files via upload

---
+++ pythonCode/wordCount.py
@@ -0,0 +1,24 @@
+from sys import argv
+import re
+import string
+frequency  = {}
+from collections import Counter
+#makes a list called wrds
+wrds = []
+print("Hello wordCount")
+#opens the txt file and reads
+f= open(r"declaration.txt","r",encoding="utf-8-sig")
+#makes a file called wordcountkey and reads and write
+fout = open("wordcountkey.txt","w+")
+#reads lines from declarations stored in varible f
+for line in f.readlines():
+    for word in line.split():
+         wrds.append(word.lower())
+wrds.sort()
+count= Counter(wrds)
+
+
+for item in count.items():
+
+    fout.write("{}\t{}\n".format(*item))
+f.close()
